1d066b470c240dfcc0f2bdc2c250605fa0409410
nuhkoca <nuhkocaa@gmail.com>
2018-06-26 15:01:42 +0300

    switch to DataBinding v2

--- app/src/main/java/com/nuhkoca/udacitybakingapp/widget/BakingWidgetProvider.java
+++ app/src/main/java/com/nuhkoca/udacitybakingapp/widget/BakingWidgetProvider.java
@@ -9,7 +9,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.RemoteViews;
 
 import com.nuhkoca.udacitybakingapp.R;
